7634149d3fc88a460530c8c07bd63f312ed059c6
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-13 20:28:52 +0900

    Rollup + TypeScript + React = ❤️

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.ts', '.tsx'];
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    file: 'public/index.js',
+    format: 'iife',
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    resolve({
+      extensions,
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel({
+      extensions,
+      exclude: /node_modules/,
+      babelrc: false,
+      runtimeHelpers: true,
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        'react-require',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-object-rest-spread', {
+          useBuiltIns: true,
+        }],
+        ['@babel/plugin-transform-runtime', {
+          corejs: 2,
+          helpers: true,
+          regenerator: true,
+          useESModules: false,
+        }],
+      ],
+    }),
+    terser(),
+  ],
+};

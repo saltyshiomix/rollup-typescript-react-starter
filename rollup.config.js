c5603601433566cc91ba075c40ec98134634be17
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2020-01-10 21:46:16 +0900

    Use latest rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import replace from '@rollup/plugin-replace';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
